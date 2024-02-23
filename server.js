a8cf6b04d5c2f43c2ad59edc0281fcc316fce8dd
Pongsagon <156410528+Bankpg@users.noreply.github.com>
2024-02-23 10:26:48 +0700

    body-parser

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ app.get('/books/:id', (req, res) => {
   res.json(books.find(book => book.id === req.params.id))
 })
 
-// Insert into books (id,name) values ("3","Crazy horse")
-app.post('/books', (req, res) => {
-  books.push(req.body)
-  res.status(201).json(req.body)
-})
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
