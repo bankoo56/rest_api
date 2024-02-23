7b7f6779bad9f058c23fd45e8d3afba55a292d68
Pongsagon <156410528+Bankpg@users.noreply.github.com>
2024-02-23 10:21:02 +0700

    db.json

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
+
+// Select * from books
+const books = require('./db')
+app.get('/books', (req, res) => {
+  res.json(books)
+})
+
+// Select * from books where id = 1?
+app.get('/books/:id', (req, res) => {
+  res.json(books.find(book => book.id === req.params.id))
+})
+
+// Insert into books (id,name) values ("3","Crazy horse")
+app.post('/books', (req, res) => {
+  books.push(req.body)
+  res.status(201).json(req.body)
+})
